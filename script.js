83637020bb3adef4d5ff54c1f7e88daa5e015bb1
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-05-22 18:48:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-
+let ax, ay, az;
+let subcanvas;
 
 function setup(){
+
     createCanvas(windowWidth, windowHeight);
+
+    subcanvas = createGraphics(800, 800, WEBGL);
 }
 
 function draw(){
+
     background(255);
 
+    ax = rotationX / 360 * 2 * PI;
+    ay = rotationY / 360 * 2 * PI;
+    az = rotationZ / 360 * 2 * PI;
 
     fill(0);
     textSize(40);
@@ -18,6 +26,20 @@ function draw(){
 
     text(rotationZ, 30, 300);
 
+    text(123, 40, 50);
+
     //text(accelerationX, 30, 200);
+
+
+    subcanvas.clear();
+    subcanvas.background(220);
+
+    subcanvas.rotateZ(az)
+    subcanvas.box(200);
+
+    image(subcanvas, 30, 400);
+
+    
+    
 
 }
