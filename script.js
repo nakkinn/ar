4034684eee6335694c685b0cd89da7d3990a23f2
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-05-25 14:00:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,108 +1,54 @@
-// let subcanvas;
+const button1 = document.getElementById('button1');
+button1.addEventListener('click',()=>{
+    room.send('abc');
+});
+
+//Peer作成
+const peer = new Peer({
+    key: 'cf1155ef-ab9f-41a3-bd4a-b99c30cc0663',
+    debug: 2
+});
+
+
+peer.on('open',()=>{
+    room=peer.joinRoom("may25",{
+        mode:'sfu'
+    });
+    room.on('open',()=>{
+        console.log('join');
+    });
+    room.on('peerJoin',peerId=>{
+        console.log(peerId+"参加");
+    });
+    room.on('peerLeave',peerId=>{
+        console.log(peerId+"退出");
+    });
+    room.on('data',message=>{
+        receive(message.data);
+    });
+});
 
 
 
-// const renderer = new THREE.WebGLRenderer({
-//     canvas:document.querySelector('#subcanvas')
-// });
-// renderer.setPixelRatio(window.devicePixelRatio);
-// renderer.setSize(800, 800);
-// renderer.setClearColor(0xbbbbbb);
-
-
-// const scene = new THREE.Scene();
-
-
-// const camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
-// camera.position.set(0, 0, 50);
 
 
 
-// const geometry = new THREE.BoxGeometry(10, 10, 10);
-// const material = new THREE.MeshNormalMaterial();
-// const cube = new THREE.Mesh(geometry, material);
 
+function receive(arg){
+    console.log(arg);
+}
 
-// scene.add(cube);
-
-
-// function setup(){
-
-//     createCanvas(windowWidth, windowHeight);
-
-// }
-
-// function draw(){
-
-//     background(255);
-
-
-//     // let ax = rotationX / 360 * 2 * PI;
-//     // let ay = rotationY / 360 * 2 * PI;
-//     //let az = rotationZ / 360 * 2 * PI;
-
-    
-//     let el = new THREE.Euler(0, PI/4, 0.4);
-
-//     cube.setRotationFromEuler(el);
-
-//     renderer.render(scene, camera);
-
-// }
-
-
-
-let ax, ay, az;
-let subcanvas;
 
 function setup(){
-
     createCanvas(windowWidth, windowHeight);
 
-    subcanvas = createGraphics(800, 800, WEBGL);
 }
 
 function draw(){
-
     background(255);
+    if(mouseIsPressed)  background(255);
 
-    ax = rotationX / 360 * 2 * PI;
-    ay = rotationY / 360 * 2 * PI;
-    az = rotationZ / 360 * 2 * PI;
-
-    fill(0);
+    fill(255, 0, 0);
     textSize(40);
-
-
-    text(rotationX, 30, 100);
-
-    text(rotationY, 30, 200);
-
-    text(rotationZ, 30, 300);
-
-
-
-    //text(accelerationX, 30, 200);
-
-
-    subcanvas.clear();
-    subcanvas.background(220);
-
-    subcanvas.push();
-
-    
-    subcanvas.rotateX(ax);
-    subcanvas.rotateZ(az);
-
-
-
-    subcanvas.box(200);
-
-    subcanvas.pop();
-
-    image(subcanvas, 30, 400);
-
-    
-    
-
+    text('ver1.00', 100, 100);
 }
