2f444516ebb38432ee44b7c58e2f85f2e4f6d380
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-05-26 21:53:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,20 +25,30 @@ peer.on('open',()=>{
 
 
 
+let posx=0, posy=0, posz=0;
+let vx=0, vy=0, vz=0;
 
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    frameRate(6);
 }
 
 function draw(){
     background(255);
 
-    room.send(rotationX.toFixed(4) + ',' + rotationY.toFixed(4) + ',' + rotationZ.toFixed(4));
+
+    vx += accelerationX;
+    vy += accelerationY;
+    vz += accelerationZ;
+
+    posx += vx*deltaTime;
+    posy += vy*deltaTime;
+    posz += vz*deltaTime;
+
+    if(frameCount%10==0)    room.send(rotationX.toFixed(4) + ',' + rotationY.toFixed(4) + ',' + rotationZ.toFixed(4) + ',' + posx.toFixed(4) + ',' + posy.toFixed(4) + ',' + posz.toFixed(4));
     
     
     fill(255, 0, 0);
     textSize(40);
-    text('ver1.09', 100, 100);
+    text('ver1.10', 100, 100);
 }
