a04b186a31b1060e20ab68d577f772c307890238
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-10-16 11:50:20 +0900

    Add files via upload

--- common_cgweb2_v2.js
+++ common_cgweb2_v2.js
@@ -256,10 +256,27 @@ document.addEventListener('wheel', function(event) {
 document.addEventListener('mousemove', (event)=>{   //第1引数　'click'：ページをクリックすると発火, 'mousemove'：異なる要素にマウスが移動すると発火
     if(event.target.tagName.toLowerCase()=='canvas'){   //クリック位置（移動先）がキャンバス要素のとき
         canvasover = true;  //キャンバス操作オン
-        //document.body.style.overflow = 'hidden';    //スクロールを無効にする
+        document.body.style.overflow = 'hidden';    //スクロールを無効にする
+        document.body.style.backgroundColor = 'lightgreen';
     }else{   //クリック位置（移動先）がキャンバス要素でないとき
         canvasover = false;  //キャンバス操作オフ
-        //document.body.style.overflow = '';  //スクロールを有効にする
+        document.body.style.overflow = '';  //スクロールを有効にする
+        document.body.style.backgroundColor = 'white';
+    }
+})
+
+
+
+//キャンバス上で操作しているか否かの切り替え
+document.addEventListener('pointermove', (event)=>{   //第1引数　'click'：ページをクリックすると発火, 'mousemove'：異なる要素にマウスが移動すると発火
+    if(event.target.tagName.toLowerCase()=='canvas'){   //クリック位置（移動先）がキャンバス要素のとき
+        canvasover = true;  //キャンバス操作オン
+        document.body.style.overflow = 'hidden';    //スクロールを無効にする
+        document.body.style.backgroundColor = 'lightgreen';
+    }else{   //クリック位置（移動先）がキャンバス要素でないとき
+        canvasover = false;  //キャンバス操作オフ
+        document.body.style.overflow = '';  //スクロールを有効にする
+        document.body.style.backgroundColor = 'white';
     }
 })
 
